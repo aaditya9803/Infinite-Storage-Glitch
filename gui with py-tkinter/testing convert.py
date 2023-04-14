088f7c0f68d6e473d3746d9c37d6b313283f2e50
aaditya9803 <aaditya9803@gmail.com>
2023-04-15 04:50:17 +0545

    Revert "step 1 - testing conversion to np array"
    This reverts commit f7a8e69496a6ce14f524d3365226c970f68f9cbc.

--- gui with py-tkinter/testing convert.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-with open('power.png', 'rb') as file:
-    binary_data = file.read()
-binary_array = np.unpackbits(np.array(bytearray(binary_data), dtype=np.uint8))
-np.save('binary_array.npy', binary_array)
